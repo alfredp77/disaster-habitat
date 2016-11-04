c6e87728381460acf559140747178dd4c3e4d330
Alfred Padawangi <alfred@asiaexpat.com.sg>
2016-11-05 07:47:37 +0800

    fixed all build errors

--- app/Kastil/Kastil.iOS/Bootstrap/FilePluginBootstrap.cs
+++ app/Kastil/Kastil.iOS/Bootstrap/FilePluginBootstrap.cs
@@ -1,6 +1,6 @@
 using MvvmCross.Platform.Plugins;
 
-namespace Tap2Give.iOS.Bootstrap
+namespace Kastil.iOS.Bootstrap
 {
     public class FilePluginBootstrap
         : MvxLoaderPluginBootstrapAction<MvvmCross.Plugins.File.PluginLoader, MvvmCross.Plugins.File.iOS.Plugin>
