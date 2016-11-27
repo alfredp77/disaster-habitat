1038c20c380ccbeaf5e2d8a38d306bb5e167b30d
Alfred Padawangi <alfred@asiaexpat.com.sg>
2016-11-27 17:07:49 +0800

    change ios app icon

--- app/Kastil/Kastil.Core/Services/SyncService.cs
+++ app/Kastil/Kastil.Core/Services/SyncService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Kastil.Common.Models;
@@ -10,14 +11,26 @@ namespace Kastil.Core.Services
         private ITap2HelpService Tap2HelpService => Resolve<ITap2HelpService>();
         private IPullService PullService => Resolve<IPullService>();
         private IPushService PushService => Resolve<IPushService>();
+		private IPersistenceContextFactory ContextFactory => Resolve<IPersistenceContextFactory>();
 
         public async Task Sync(string userToken)
         {
+			var currentTime = DateTimeOffset.UtcNow;
+
             await PullDisasters();
             await PullAttributes();
             await PushAssessments(userToken);
             await PushShelters(userToken);
+
+			RecordLastSync(currentTime);
         }
+
+		private void RecordLastSync(DateTimeOffset currentTime)
+		{
+			var syncInfo = new SyncInfo { ObjectId = "x", LastSync = currentTime };
+			var context = ContextFactory.CreateFor<SyncInfo>();
+			context.Save(syncInfo);
+		}
 
         public async Task PullDisasters()
         {
@@ -33,7 +46,7 @@ namespace Kastil.Core.Services
 
         private async Task PullAttributes()
         {
-            await PullService.Pull<Attribute>(true);
+            await PullService.Pull<Common.Models.Attribute>(true);
         }
 
         private async Task PushShelters(string userToken)
