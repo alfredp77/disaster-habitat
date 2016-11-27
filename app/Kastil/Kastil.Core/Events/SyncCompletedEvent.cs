1be2b2a5aa7c8f7c553aa54ba2fa4b806ba95ecc
Alfred Padawangi <alfred@asiaexpat.com.sg>
2016-11-27 15:22:43 +0800

    remove xcode-based syncview

---
+++ app/Kastil/Kastil.Core/Events/SyncCompletedEvent.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.Plugins.Messenger;
+
+namespace Kastil.Core.Events
+{
+
+	public class SyncCompletedEvent : MvxMessage
+	{
+		
+	}
+}
