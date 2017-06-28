cf7842d27691bf8603bea3f1ec06c155ed5eb69e
Alfred Padawangi <alfred@asiaexpat.com.sg>
2017-06-29 00:01:32 +0800

    prepare apk

--- app/Kastil/Kastil.Common/Connection.cs
+++ app/Kastil/Kastil.Common/Connection.cs
@@ -3,20 +3,25 @@ using Kastil.Common.Models;
 
 namespace Kastil.Common
 {
-    public partial class Connection
-    {
-        public string AppId { get; set; }
-        public string SecretKey { get; set; }
+	public partial class Connection
+	{
+		public string AppId { get; set; }
+		public string SecretKey { get; set; }
 
-        private static string APPID_HEADER = "application-id";
-        private static string SECRET_KEY_HEADER = "secret-key";
-        private static string APPLICATION_TYPE_HEADER = "application-type";
-        private static string REST = "REST";
-        private static string CONTENT_TYPE_HEADER = "Content-Type";
-        private static string CONTENT_TYPE = "application/json";
-        private static string BASE_URL = "https://api.backendless.com/v1";
+		private static string APPID_HEADER = "application-id";
+		private static string SECRET_KEY_HEADER = "secret-key";
+		private static string APPLICATION_TYPE_HEADER = "application-type";
+		private static string REST = "REST";
+		private static string CONTENT_TYPE_HEADER = "Content-Type";
+		private static string CONTENT_TYPE = "application/json";
+#if DEBUG
+		private static string BASE_URL = "https://api.backendless.com/v1";
 		private static string BASE_NON_HTTPS_URL = "http://api.backendless.com/v1";
-        private static string USER_TOKEN = "user-token";
+#else
+		private static string BASE_URL = "https://api.backendless.com/vprod";
+		private static string BASE_NON_HTTPS_URL = "http://api.backendless.com/vprod";
+#endif
+		private static string USER_TOKEN = "user-token";
 
         private Dictionary<string, string> _headers; 
         public Dictionary<string, string> Headers => _headers ?? (_headers = new Dictionary<string, string>
